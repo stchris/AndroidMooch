d9e86e4381dc9bb82fdf99570c9ff210ba14bc94
Christian Stefanescu <st.chris@gmail.com>
2011-10-09 14:12:28 +0200

    Renamed activities. Now the flow is: BookSearchActivity -> BookSearchListActivity.

--- src/com/github/stchris/activities/BookSearchActivity.java
+++ src/com/github/stchris/activities/BookSearchActivity.java
@@ -1,68 +1,67 @@
 package com.github.stchris.activities;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-import com.github.stchris.models.Book;
-import com.github.stchris.models.BookListAdapter;
+import com.github.stchris.R;
 import com.github.stchris.queries.Constants;
-import com.github.stchris.util.JSONUtil;
 
-public class BookSearchActivity extends ListActivity {
+/**
+ * Main AndroidMooch {@link Activity}.
+ * 
+ * @author chris
+ * 
+ */
+public class BookSearchActivity extends Activity {
 
-	private Book[] books;
-
-	/**
-	 * Intent action for BookSearchActivity.
-	 */
-	public static final String ACTION = "com.github.stchris.action.BookSearchAction";
-
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-		String searchString = "";
-		String searchEngine = "";
-		String searchEngineCode = "";
-		final Intent intent = getIntent();
-		if (ACTION.equals(intent.getAction())) {
-			searchString = intent.getExtras()
-					.getString(Constants.SEARCH_STRING);
-			searchEngine = intent.getExtras()
-					.getString(Constants.SEARCH_ENGINE);
-		}
-		
-		if (searchEngine.equals("BookMooch")) {
-			searchEngineCode = "bm"; 
-		} else if (searchEngine.equals("Amazon.com")) {
-			searchEngineCode = "amazon.com";
-		} else if (searchEngine.equals("Amazon.de")) {
-			searchEngineCode = "amazon.de";
-		} else if (searchEngine.equals("Amazon.co.uk")) {
-			searchEngineCode = "amazon.co.uk";
-		} else if (searchEngine.equals("Amazon.co.jp")) {
-			searchEngineCode = "amazon.co.jp";
-		} else if (searchEngine.equals("Amazon.fr")) {
-			searchEngineCode = "amazon.fr";
-		} else if (searchEngine.equals("Amazon.ca")) {
-			searchEngineCode = "amazon.ca";
-		}
+		final EditText isbnText = (EditText) findViewById(R.id.isbntext);
+		final Button btnBookInfo = (Button) findViewById(R.id.btnBookInfo);
+		final EditText searchText = (EditText) findViewById(R.id.searchtext);
+		final Button btnSearchBook = (Button) findViewById(R.id.btnSearchBook);
 
-		books = JSONUtil.searchBooks(searchString, searchEngineCode);
-		setListAdapter(new BookListAdapter(this, books));
-		getListView().setTextFilterEnabled(true);
+		final Spinner spinnerSearchEngine = (Spinner) findViewById(R.id.spinnerSearchEngine);
+		SpinnerAdapter searchEngineSpinnerAdapter = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item, new String[] {
+						"BookMooch", "Amazon.com", "Amazon.de", "Amazon.co.uk",
+						"Amazon.co.jp", "Amazon.fr", "Amazon.fr" });
+		spinnerSearchEngine.setAdapter(searchEngineSpinnerAdapter);
+
+		btnBookInfo.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Intent bookInfoIntent = new Intent(BookInfoActivity.ACTION);
+				bookInfoIntent.putExtra(Constants.ISBN_PARAMETER, isbnText
+						.getText().toString());
+				startActivity(bookInfoIntent);
+			}
+
+		});
+
+		btnSearchBook.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				Intent bookSearchIntent = new Intent(BookSearchListActivity.ACTION);
+				bookSearchIntent.putExtra(Constants.SEARCH_STRING, searchText
+						.getText().toString());
+				bookSearchIntent.putExtra(Constants.SEARCH_ENGINE,
+						spinnerSearchEngine.getSelectedItem()
+								.toString());
+				startActivity(bookSearchIntent);
+			}
+		});
 	}
-
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-
-		Intent bookInfoIntent = new Intent(BookInfoActivity.ACTION);
-		bookInfoIntent.putExtra(Constants.ISBN_PARAMETER, books[position].isbn);
-		startActivity(bookInfoIntent);
-	}
-
 }
---
+++ src/com/github/stchris/activities/BookSearchListActivity.java
@@ -0,0 +1,68 @@
+package com.github.stchris.activities;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.github.stchris.models.Book;
+import com.github.stchris.models.BookListAdapter;
+import com.github.stchris.queries.Constants;
+import com.github.stchris.util.JSONUtil;
+
+public class BookSearchListActivity extends ListActivity {
+
+	private Book[] books;
+
+	/**
+	 * Intent action for BookSearchActivity.
+	 */
+	public static final String ACTION = "com.github.stchris.action.BookSearchAction";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		String searchString = "";
+		String searchEngine = "";
+		String searchEngineCode = "";
+		final Intent intent = getIntent();
+		if (ACTION.equals(intent.getAction())) {
+			searchString = intent.getExtras()
+					.getString(Constants.SEARCH_STRING);
+			searchEngine = intent.getExtras()
+					.getString(Constants.SEARCH_ENGINE);
+		}
+		
+		if (searchEngine.equals("BookMooch")) {
+			searchEngineCode = "bm"; 
+		} else if (searchEngine.equals("Amazon.com")) {
+			searchEngineCode = "amazon.com";
+		} else if (searchEngine.equals("Amazon.de")) {
+			searchEngineCode = "amazon.de";
+		} else if (searchEngine.equals("Amazon.co.uk")) {
+			searchEngineCode = "amazon.co.uk";
+		} else if (searchEngine.equals("Amazon.co.jp")) {
+			searchEngineCode = "amazon.co.jp";
+		} else if (searchEngine.equals("Amazon.fr")) {
+			searchEngineCode = "amazon.fr";
+		} else if (searchEngine.equals("Amazon.ca")) {
+			searchEngineCode = "amazon.ca";
+		}
+
+		books = JSONUtil.searchBooks(searchString, searchEngineCode);
+		setListAdapter(new BookListAdapter(this, books));
+		getListView().setTextFilterEnabled(true);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+
+		Intent bookInfoIntent = new Intent(BookInfoActivity.ACTION);
+		bookInfoIntent.putExtra(Constants.ISBN_PARAMETER, books[position].isbn);
+		startActivity(bookInfoIntent);
+	}
+
+}
